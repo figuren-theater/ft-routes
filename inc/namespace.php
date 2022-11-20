c761f812a8160a176a6b8991eb35d4dd915022f7
carstenbach <mail@carsten-bach.de>
2022-11-20 04:31:44 +0100

    WIP

--- inc/namespace.php
+++ inc/namespace.php
@@ -21,6 +21,7 @@ function register() {
 	$default_settings = [
 		'enabled' => true, // needs to be set
 	];
+	
 	$options = [
 		'defaults' => $default_settings,
 	];
--- templates/htdocs/wp-content/sunrise.php
+++ templates/htdocs/wp-content/sunrise.php
@@ -14,6 +14,8 @@
 
 declare(strict_types=1);
 
+$_ft_mercator_path = FT_VENDOR_DIR . '/humanmade/';
+
 /**
  * When trying to log in with an aliased domain, cookie domain is incorrect. 
  * Probably Mercator\SSO\initialize_cookie_domain needs to run whether SSO is disabled or not,
@@ -25,6 +27,9 @@ declare(strict_types=1);
 add_filter( 'mercator.sso.enabled', '__return_false' );
 add_action( 'muplugins_loaded', 'Mercator\\SSO\\initialize_cookie_domain' );
 
+
 // NOTE include gui.php before mercator.php itself!
-require WPMU_PLUGIN_DIR . '/mercator-gui/gui.php';
-require WPMU_PLUGIN_DIR . '/mercator/mercator.php';
+require $_ft_mercator_path . 'mercator-gui/gui.php';
+require $_ft_mercator_path . 'mercator/mercator.php';
+
+unset( $_ft_mercator_path );
